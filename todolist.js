13cfde0c80f082214ddf481ad21b488c7bda1f03
Skylar Yarter <soy1@williams.edu>
2025-05-08 19:07:03 -0400

    to do list fully functional

--- todolist.js
+++ todolist.js
@@ -1,11 +1,8 @@
 // functions for first task
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-
-checkboxes.forEach(checkbox => {
-    checkbox.addEventListener('click', function() {
-        numChecked();
-        checkbox.parentNode.classList.add('hide');
-    });
+const firstchk = document.getElementById('firstchk');
+firstchk.addEventListener('click', function() {
+    numChecked();
+    firstchk.parentNode.classList.add('hide');
 });
 
 function numChecked() {
@@ -15,39 +12,68 @@ function numChecked() {
     tracker.textContent = `Tasks Completed: ${numCompleted}`;
 }
 
-const editable = document.getElementsByClassName("editable");
-for (let text of editable) {
-    text.addEventListener('focus', () => {
-        text.classList.add('edited');
-        text.classList.remove('editable');
-    });
-}
+var first = document.getElementById("first");
+first.addEventListener('focus', () => {
+    document.getElementById("firstchk").disabled = false;
+    first.classList.add('edited');
+    first.classList.remove('editable');
+    addTask();
+}, { once: true });
+
+// close button
+const xButton = document.getElementById("x");
+first.addEventListener('focus', () => {
+    xButton.style = "display: inline-block";
+});
+first.addEventListener('blur', () => {
+    xButton.style = "display: none";
+});
+xButton.addEventListener('mousedown', () => {
+    xButton.parentNode.classList.add('hide');
+});
 
 // sets up a new task and adds event listeners to the new content
 function addTask() {
     var btn = document.getElementById('addTask');
 
     var div = document.createElement('div');
-    div.setAttribute('class', 'form-check input-group-addon');
+    div.setAttribute('class', 'form-check input-group-addon chkbox');
     btn.parentNode.insertBefore(div, btn); 
 
     var chk = document.createElement("input"); 
     chk.setAttribute('type', 'checkbox');
     chk.setAttribute('class', 'form-check-input')
+    chk.setAttribute('disabled', true);
     chk.addEventListener('click', function() {
         numChecked();
         div.classList.add('hide');
     });
   
     var txt = document.createElement("p");
-    txt.setAttribute('class', 'editable');
+    txt.setAttribute('class', 'editable textbox');
     txt.setAttribute('contenteditable', 'true');
     txt.textContent = "Click to edit";
     txt.addEventListener('focus', () => {
+        chk.disabled = false;
         txt.classList.add('edited');
         txt.classList.remove('editable');
+        addTask();
+    }, { once: true });
+
+    var x = document.createElement("button");
+    x.setAttribute('class', 'btn-close');
+    x.setAttribute('style', 'display: none');
+    txt.addEventListener('focus', () => {
+        x.style = "display: inline-block";
+    });
+    txt.addEventListener('blur', () => {
+        x.style = "display: none";
+    });
+    x.addEventListener('mousedown', () => {
+        div.classList.add('hide');
     });
 
     div.appendChild(chk);
     div.appendChild(txt);
+    div.appendChild(x);
 }
